5ba905c91570d02820b2a856a7f3f77bf865d445
Olivier Friard <olivier.friard@unito.it>
2021-10-01 11:23:19 +0200

    some work

---
+++ gisaid_provision-json.py
@@ -0,0 +1,15 @@
+"""
+extract metadata from provision.json file from GISAID
+"""
+
+import sys
+import pathlib as pl
+
+if not pl.Path(sys.argv[1]).is_file:
+    print("file not found")
+    sys.exit()
+
+with open(sys.argv[1], "r") as f_in:
+    print(f_in.readline())
+    break
+
