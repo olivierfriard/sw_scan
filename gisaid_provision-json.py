550ea062a6757b118ec91f8891f81f9a4ed790b2
Olivier Friard <olivier.friard@unito.it>
2021-10-01 11:26:07 +0200

    some work

--- gisaid_provision-json.py
+++ gisaid_provision-json.py
@@ -3,6 +3,7 @@ extract metadata from provision.json file from GISAID
 """
 
 import sys
+import json
 import pathlib as pl
 
 if not pl.Path(sys.argv[1]).is_file:
@@ -12,6 +13,8 @@ if not pl.Path(sys.argv[1]).is_file:
 with open(sys.argv[1], "r") as f_in:
     for line in f_in:
         print(line)
+        line_from_json = json.loads(line)
+        print(line_from_json)
         break
     
 
