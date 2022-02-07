e6a0cc10db5a658ddd2d7da82db1bf23c3cc1893
Olivier Friard <olivier.friard@unito.it>
2022-02-07 15:18:48 +0100

    some changes

--- sw_scan_ui.py
+++ sw_scan_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'sw_scan.ui2'
+# Form implementation generated from reading ui file 'sw_scan.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
